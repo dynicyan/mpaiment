570b4599d90d80e36c08c477de0406696025e641
dynicyan <dynicyan@qq.com>
2017-07-19 18:03:16 +0800

    moedei

---
+++ src/packages/mpaiCheck/CheckBox.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div(:class="[type === 'radio' ? 'mp-radios' : 'mp-checks'],
+    {'is-disabled': 'disabled', }")
+</template>
+<script>
+export default {
+    name: mpCheck,
+    props: {
+        type: {
+            type: String,
+            default: 'check'
+        }
+    }
+}
+</script>
+<style lang="stylus">
+</style>
---
+++ src/packages/mpaiNodata/noData.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+  .mp_data
+    .img_box
+      img(:src='imgSrc')
+      slot(name='action')
+    .text {{text}}
+</template>
+
+<style lang="stylus">
+  .mp_data
+    width 100%
+    margin 0 auto
+    min-height 100px
+    .text
+      margin-top 10px
+      text-align center
+      font-size 20px
+      color #888
+      font-family PingFangSC
+    .img_box
+      width 206px
+      height 136px
+      margin 0 auto
+      position relative
+      img
+        position absolute
+        top 0
+        left 0
+        bottom 0
+        right 0
+        margin auto
+        width auto
+        height auto
+        max-width 100%
+        max-height 100%
+</style>
+
+<script>
+  export default {
+    props: {
+      text: {
+        type: String,
+        default: '暂无数据'
+      },
+      imgSrc: {
+        type: String,
+        default: ''
+      }
+    }
+  }
+</script>
