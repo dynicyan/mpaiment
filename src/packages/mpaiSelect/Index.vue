1d632419a2522938771ba2d7da1fffda8b5b1465
luckyJessie <https://github.com/dynicyan/mpaiment.git>
2017-08-07 17:08:06 +0800

    add local files

--- src/packages/mpaiSelect/Index.vue
+++ src/packages/mpaiSelect/Index.vue
@@ -1,19 +1,72 @@
 <template lang="pug">
-  .hello
+  .selectBox
     h1 {{ msg }}
-    input(value='select')
+    .showList
+      p 一般选择器
+      mp-select(type="primary" @change='change' :val='value1')
+      mp-option(v-for="option in options" :key='option.key' :val='option.value')
+    .showList
+      p 可清除选择器
+      mp-select(type="primary" clearable @change='change' :val='value2')
+      mp-option(v-for="option in options" :key='option.key' :val='option.value')
+    .showList
+      p radio单选按钮选择器
+      mp-select(type="radio" @change='change' :val='value3')
+        mp-option(v-for="option in options" :key='option.key' :val='option.value')
+    .showList
+      p 多选选择器
+      mp-select(type="multiple" @change='change' :val='value4')
+        mp-option(v-for="option in options" :key='option.key' :val='option.value')
+    .showList
+      p 多选可输入选择器
+      mp-select(type="multiple" editable @change='change' :val='value5')
+        mp-option(v-for="option in options"  :key='option.key' :val='option.value')
+    .showList
+      p 多选key值
+      mp-select(type="multiple" editable @change='change' :val='value6')
+        mp-option(v-for="option in options" :key='option.key' :val='option.value')
 </template>
-
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'mPaiMent',
-      textLists: {}
+  import mpSelect from './select'
+  import mpOption from './option'
+  export default {
+    components: {
+      mpSelect,
+      mpOption
+    },
+    data () {
+      return {
+        value1: '',
+        value2: '',
+        value3: '',
+        value4: '',
+        value5: '',
+        value6: '',
+        editable: true,
+        clearable: true,
+        options: [
+          {'value': '选项1', 'key': 31313131},
+          {'value': '选项2', 'key': 31313131},
+          {'value': '选项3', 'key': 31313131},
+          {'value': '选项4', 'key': 31313131}
+        ],
+        msg: 'mPaiMent的选择器select展示'
+      }
+    },
+    methods: {
+      transfer (data) {
+      },
+      change (val) {
+        console.log(val)
+      }
     }
   }
-}
 </script>
 <style lang="stylus">
+  .selectBox
+    .showList
+      height 100px
+      p
+        height 36px
+        line-height 36px
 </style>
